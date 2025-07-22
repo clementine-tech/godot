b7a78008c95959463311d2b2183ba4689130509d
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2025-07-21 18:48:30 -0700

    small changes so that godot builds

--- build_godot_windows.sh
+++ build_godot_windows.sh
@@ -4,11 +4,11 @@ set -e
 # Clear previous builds
 scons -c
 
-# Compile Godot for linuxbsd x86_64 with mono
-scons platform=windows arch=x86_64 module_mono_enabled=yes precision=double
+# Compile Godot for Windows x64 with mono
+scons platform=windows arch=x86_64 module_mono_enabled=yes precision=double production=yes
 
 # Generate the .NET glue code
-wine ./bin/godot.windows.editor.double.x86_64.mono.exe --headless --generate-mono-glue modules/mono/glue
+./bin/godot.windows.editor.double.x86_64.mono.exe --headless --generate-mono-glue modules/mono/glue
 
 # Define the NuGet source name
 NUGET_SOURCE_NAME="GodotMonoVoxel"
@@ -17,19 +17,19 @@ NUGET_SOURCE_NAME="GodotMonoVoxel"
 if dotnet nuget list source | grep -A 1 "[[:space:]]${NUGET_SOURCE_NAME}[[:space:]]\["; then
     echo "${NUGET_SOURCE_NAME} NuGet source found."
     # Extract the existing source path from the NuGet CLI output
-    VOXEL_SOURCE_PATH=$(dotnet nuget list source | grep -A 1 "[[:space:]]${NUGET_SOURCE_NAME}[[:space:]]\[" | tail -n 1 | xargs)
+    VOXEL_SOURCE_PATH=$(dotnet nuget list source | grep -A 1 "[[:space:]]${NUGET_SOURCE_NAME}[[:space:]]\[" | tail -n 1 | sed 's/\\/\//g' | xargs)
     echo "Using existing source path: $VOXEL_SOURCE_PATH"
+    
+    # Create directory if it doesn't exist
+    mkdir -p "$VOXEL_SOURCE_PATH"
 else
     echo "${NUGET_SOURCE_NAME} NuGet source not found. Creating in bin/${NUGET_SOURCE_NAME}..."
     VOXEL_SOURCE_PATH="bin/${NUGET_SOURCE_NAME}"
     mkdir -p "$VOXEL_SOURCE_PATH"
-    ABSOLUTE_PATH="$(pwd)/${VOXEL_SOURCE_PATH}"
-    dotnet nuget add source "$ABSOLUTE_PATH" --name ${NUGET_SOURCE_NAME}
+    dotnet nuget add source "$VOXEL_SOURCE_PATH" --name ${NUGET_SOURCE_NAME}
 fi
 
 # Build the assemblies and push them to your local NuGet source
-./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin \
+python ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin \
   --push-nupkgs-local "$VOXEL_SOURCE_PATH" \
-  --precision=double
-
-chmod +x bin/godot.windows.editor.double.x86_64.mono.exe
+  --precision=double 
